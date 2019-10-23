1219e6e24da369eb18af66c361ad1e6bf90eb94f
david <david.ramirez-mo@mail.escuelaing.edu.co>
2019-10-23 18:52:45 -0500

    token

--- client/src/main/resources/static/js/app.js
+++ client/src/main/resources/static/js/app.js
@@ -1,16 +1,26 @@
-app=(function(){
-    return{
-        getUsers:function(){
+app = (function () {
+    return {
+        getUsers: function () {
             console.log("asd");
-            $.get("/User",function(data){
+            $.get("/User", function (data) {
                 console.log(data);
-                
-			});
+
+            });
         },
-        login:function(){
-            
+        login: function () {
+            var nom = document.getElementById("name").value;
+            var pass = document.getElementById("password").value;
+            var user = { name: nom, password: pass };
+            axios.post("login",user).then(function(response){
+                console.log(response.data.accessToken);
+                localStorage.setItem("token",response.data.accessToken);
+            }).catch(function (error) {
+                console.log(error);
+              });
+            console.log(name);
+            console.log(pass);
         }
 
     }
-    
+
 })();
